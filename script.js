14e3e2ba0b7f36f2052dba6a840c7d2b01164444
gabriele traversa <s6072517@studenti.unige.it>
2025-11-04 16:10:54 +0100

    pausa merendina

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById("mex").onclick = function() {
+    window.location.href = "notizie/OLSOLS.html";
+};
+document.getElementById("mex2").onclick = function() {
+    window.location.href = "notizie/OLSOLS.html";
+};
+document.getElementById("mex3").onclick = function() {
+    window.location.href = "notizie/OLSOLS.html";
+};
+document.getElementById("mex4").onclick = function() {
+    window.location.href = "notizie/OLSOLS.html";
+};
+document.getElementById("mex5").onclick = function() {
+    window.location.href = "notizie/OLSOLS.html";
+};
+document.getElementById("mex6").onclick = function() {
+    window.location.href = "notizie/OLSOLS.html";
+};
+document.getElementById("mex7").onclick = function() {
+    window.location.href = "notizie/OLSOLS.html";
+};
+document.getElementById("mex8").onclick = function() {
+    window.location.href = "notizie/OLSOLS.html";
+};
+
