3b6be605cf0b2947efca9f2e617ad8c7b561bd40
gabriele traversa <s6072517@studenti.unige.it>
2025-11-05 12:14:57 +0100

    pallato frfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrrfrfrfrfrfrrfrfrfrfrfrfrfrfrfrfrfrfrfrffrffrfrfrffrfrffrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrffrfrfrfrfrffrfrfrfrfrfrfrfrfrffrfffrfrfrfrfrfrfrffrfrfrfrfrfrfrfrfrfrffrfrffrfrfrfrfrfrfrf

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 document.getElementById("mex").onclick = function() {
-    window.location.href = "notizie/OLSOLS.html";
+    window.location.href = "ultime.html";
 };
 document.getElementById("mex2").onclick = function() {
-    window.location.href = "notizie/OLSOLS.html";
+    window.location.href = "general.html";
 };
 document.getElementById("mex3").onclick = function() {
-    window.location.href = "notizie/OLSOLS.html";
+    window.location.href = "estere.html";
 };
 document.getElementById("mex4").onclick = function() {
-    window.location.href = "notizie/OLSOLS.html";
+    window.location.href = "estere.html";
 };
 document.getElementById("mex5").onclick = function() {
-    window.location.href = "notizie/OLSOLS.html";
+    window.location.href = "cultura.html";
 };
 document.getElementById("mex6").onclick = function() {
-    window.location.href = "notizie/OLSOLS.html";
+    window.location.href = "arte.html";
 };
 document.getElementById("mex7").onclick = function() {
-    window.location.href = "notizie/OLSOLS.html";
+    window.location.href = "musica.html";
 };
 document.getElementById("mex8").onclick = function() {
-    window.location.href = "notizie/OLSOLS.html";
+    window.location.href = "marassi/OLSOLS.html";
 };
-
+document.getElementById("home").onclick = function() {
+    window.location.href = "index.html";
+};
